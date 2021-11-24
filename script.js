a6ed071d82b2ea7a8c51fa41175f1958b1c01aaa
Azat Timerkaev <bust.steam@gmail.com>
2021-11-24 10:11:16 +0400

    drag & drop support

--- script.js
+++ script.js
@@ -44,14 +44,18 @@ function renderUsers() {
   for (const user of users) {
     const divToAdd = document.createElement("div");
     divToAdd.classList.add("assignee");
-    divToAdd.dataset.id = user.id;
+    divToAdd.dataset.userId = user.id;
     divToAdd.textContent = user.surname + " " + user.firstName;
+    divToAdd.addEventListener("drop", drop_handler);
+    divToAdd.addEventListener("dragover", dragover_handler);
     calendar.appendChild(divToAdd);
     for (let i = 0; i < 7; i++) {
       const divToAdd = document.createElement("div");
       divToAdd.classList.add("day");
       divToAdd.dataset.userId = user.id;
       divToAdd.dataset.day = i + 1;
+      divToAdd.addEventListener("drop", drop_handler);
+      divToAdd.addEventListener("dragover", dragover_handler);
       calendar.appendChild(divToAdd);
     }
   }
@@ -74,6 +78,8 @@ function renderTask(task, inCalendar = false) {
   taskDiv.dataset.subject = task.subject;
   taskDiv.appendChild(taskSubject);
   taskDiv.classList.add("task");
+  taskDiv.draggable = "true";
+  taskDiv.addEventListener("dragstart", dragstart_handler);
   const startDate = new Date(task.planStartDate);
   const endDate = new Date(task.planEndDate);
   if (inCalendar === true) {
@@ -83,6 +89,7 @@ function renderTask(task, inCalendar = false) {
       const calendarCell = calendar.querySelector(".day[data-user-id='" +
                            task.executor + "'][data-day='" + dayOfWeek +"']");
       const taskDivClone = taskDiv.cloneNode(true);
+      taskDivClone.addEventListener("dragstart", dragstart_handler);
       taskDivClone.dataset.date = dt.toLocaleDateString("en-US");
       calendarCell.appendChild(taskDivClone);
       if (taskDivClone.dataset.date !== document.getElementById("day"+dayOfWeek).dataset.date) {
@@ -98,6 +105,35 @@ function renderTask(task, inCalendar = false) {
   }
 }
 
+/* Drag & Drop handlers */
+function dragstart_handler(ev) {
+  ev.dataTransfer.setData("text", ev.target.dataset.taskId);
+  ev.dataTransfer.effectAllowed = "move";
+}
+
+function dragover_handler(ev) {
+  ev.preventDefault();
+  ev.dataTransfer.dropEffect = "move";
+}
+
+function drop_handler(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text");
+  const task = tasks.find(el => el.id === data);
+  task.executor = this.dataset.userId;
+  const dateDiff = ((new Date(task.planEndDate)) - (new Date(task.planStartDate)).getTime())/86400000;
+  const weekday = this.dataset.day;
+  if (weekday) {
+    const dtStr = document.getElementById("day"+weekday).dataset.date;
+    task.planStartDate = dtStr;
+    const dtObj = new Date(dtStr);
+    dtObj.setDate(dtObj.getDate()+dateDiff);
+    task.planEndDate = dtObj.toLocaleDateString("en-US");
+  }
+  document.querySelectorAll(".task[data-task-id='"+data+"']").forEach(el => {el.remove()});
+  renderTask(task, true);
+}
+
 fetch(usersRequest)
   .then(response => response.json())
   .then(data => {
@@ -106,7 +142,7 @@ fetch(usersRequest)
   })
   .catch(console.error);
 
-  fetch(tasksRequest)
+fetch(tasksRequest)
   .then(response => response.json())
   .then(data => {
     tasks = data;
